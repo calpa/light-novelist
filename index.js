6ee24f1a108cb4dddcd855f512d98af34e9a5b8a
Calpa Liu <calpaliu@gmail.com>
2017-06-05 12:58:20 +0800

    Add output sentence

--- index.js
+++ index.js
@@ -15,23 +15,36 @@ function random(array) {
   return array[Math.floor(Math.random() * len)];
 }
 
+function log(output) {
+  if (Math.random() > 0.5) {
+    console.log(output);
+  }
+}
+
 function output(total) {
   for (var i = 0; i < total; i++) {
     var pre = `${random(prefix)}，`
 
+    var setting = `${random(sentence.who)}${random(actions.observe)}${random(venue)}，${random(sentence.unexpected)}${random(sentence.actions.human)}。`
+
     var a = `${random(catchphrase.if)}，我${random(catchphrase.preRequest)}${random(actions.protect)}${random(venue)}。`;
 
-    var b = `${random(catchphrase.so)}${random(catchphrase.sorry)}，${random(sentence.nickname)}，我還不可以${random(sentence.replacement.withYou)}${random(actions.wants)}。`
+    var b = `"${random(catchphrase.so)}${random(catchphrase.sorry)}，${random(sentence.nickname)}，我還不可以${random(sentence.replacement.withYou)}${random(actions.wants)}。"`
 
     if (Math.random() > 0.5) {
       a = `${pre}${a}`;
     }
 
-    if (Math.random() > 0.5) {
-      a = `${a}${b}（${random(actions.human)}`;
-    }
+    console.log(`${i})\n${setting}\n`);
 
-    console.log(`${i}. ${a}`);
+    console.log(`"${a}"\n`);
+
+    if (Math.random() > 0.5) {
+      log(`${b}（${random(actions.human)}\n`);
+    }
+    console.log(random(sentence.suffix.end));
+    console.log("﹣﹣﹣﹣﹣﹣﹣﹣﹣﹣﹣﹣﹣﹣﹣﹣﹣﹣﹣﹣");
+
   }
 }
 
